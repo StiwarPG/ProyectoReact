cfa50eea02bfc2f971b0392861ce485763be3d1e
StiwarPG <pjhonatan235@gmail.com>
2024-11-18 14:42:41 -0500

    Noup

--- TrabajoFinal/src/main.jsx
+++ TrabajoFinal/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import  {AppMultiple} from './basquet/basquet'
+import App from './App';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <>
-      <AppMultiple/>
+      <App/>
   </>
 );
