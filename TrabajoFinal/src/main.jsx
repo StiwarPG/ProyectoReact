b13ae578ce117dc2c231f08713135679a3b54ea2
StiwarPG <pjhonatan235@gmail.com>
2024-11-18 23:01:31 -0500

    Funcion

--- TrabajoFinal/src/main.jsx
+++ TrabajoFinal/src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import  {AppMultiple} from './basquet/basquet'
-import App from './App';
-import './index.css'
 import  {AppMultiple} from './basquet/petos/basquet.jsx'
 
 createRoot(document.getElementById('root')).render(
